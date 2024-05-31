36f5ae0ec0e698f022f53651c1446f5997419f92
Thibo Kuijpers <btwthibo@gmail.com>
2024-05-31 12:43:06 +0200

    commit

--- src/components/user/services/user.service.ts
+++ src/components/user/services/user.service.ts
@@ -20,7 +20,6 @@ export async function userService(id: string) {
             products: {
               select: {
                 name: true,
-
                 price: true,
               },
             },
